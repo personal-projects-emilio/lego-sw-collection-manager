d1a959c1ac3f2055d340aba03c976012cecc96bf
Emilio Cadario <Emilio.Cadario@gmail.com>
2019-05-02 14:02:58 +0200

    Add Management and rework flex in Minifig

---
+++ src/components/pages/minifigs/minifig/management/Management.js
@@ -0,0 +1,36 @@
+import React from "react";
+import PropTypes from 'prop-types';
+import { Grid, Checkbox, IconButton, Icon, Tooltip } from "@material-ui/core";
+
+export const management = props => {
+    const { details } = props;
+  return (
+    <Grid container justify="space-evenly">
+      <Tooltip title="Possession" aria-label="Possession">
+        <Checkbox checked={!!details.possessed} color="default" />
+      </Tooltip>
+      <Tooltip title="Edit" aria-label="Edit">
+        <IconButton>
+          <Icon>edit</Icon>
+        </IconButton>
+      </Tooltip>
+      <Tooltip title="Delete" aria-label="Delete">
+        <IconButton>
+          <Icon>delete</Icon>
+        </IconButton>
+      </Tooltip>
+    </Grid>
+  )
+}
+
+management.propTypes = {
+    reference: PropTypes.string.isRequired,
+    details: PropTypes.shape({
+        characterName: PropTypes.string.isRequired,
+        name: PropTypes.string.isRequired,
+        possessed: PropTypes.bool.isRequired,
+        tags: PropTypes.arrayOf(PropTypes.string)
+    })
+}
+
+export default management;
