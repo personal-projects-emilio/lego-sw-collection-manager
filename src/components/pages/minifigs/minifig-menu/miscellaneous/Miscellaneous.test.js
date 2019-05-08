7e1632c2a48fbb5e4ebb982cd20b0be9290b06f0
Emilio Cadario <Emilio.Cadario@gmail.com>
2019-05-08 19:01:44 +0200

    Add the set possession to all buttons

--- src/components/pages/minifigs/minifig-menu/miscellaneous/Miscellaneous.test.js
+++ src/components/pages/minifigs/minifig-menu/miscellaneous/Miscellaneous.test.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import Miscellaneous from './Miscellaneous';
 import { shallow } from 'enzyme';
-import { Typography, LinearProgress, Grid, Tooltip, Button, Icon } from "@material-ui/core";
+import { Typography, LinearProgress, Grid, Tooltip, Button, Icon, Divider, Fab } from "@material-ui/core";
 
 describe('<Miscellaneous />', () => {
-  it('should render the Minifig Menu', () => {
+  it('should render the miscellaneous part of the minifig menu', () => {
     const props = {
       totalNumber: 1000,
       numberOwned: 900,
@@ -16,11 +16,13 @@ describe('<Miscellaneous />', () => {
       }
     }
     const wrapper = shallow(<Miscellaneous {...props} />);
-    expect(wrapper.find(Grid)).toHaveLength(3);
-    expect(wrapper.find(Typography)).toHaveLength(1);
+    expect(wrapper.find(Grid)).toHaveLength(4);
+    expect(wrapper.find(Typography)).toHaveLength(2);
     expect(wrapper.find(LinearProgress)).toHaveLength(1);
-    expect(wrapper.find(Tooltip)).toHaveLength(1);
+    expect(wrapper.find(Tooltip)).toHaveLength(3);
     expect(wrapper.find(Button)).toHaveLength(2);
-    expect(wrapper.find(Icon)).toHaveLength(2);
+    expect(wrapper.find(Icon)).toHaveLength(4);
+    expect(wrapper.find(Divider)).toHaveLength(2);
+    expect(wrapper.find(Fab)).toHaveLength(2);
   });
 });
