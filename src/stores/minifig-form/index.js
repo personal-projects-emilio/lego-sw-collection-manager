42e37a676440991f314eebe2bdcea2393d56290b
Emilio Cadario <Emilio.Cadario@gmail.com>
2019-05-09 00:20:45 +0200

    Add minifigForm store and template

---
+++ src/stores/minifig-form/index.js
@@ -0,0 +1,8 @@
+import minifigFormReducer from './reducer';
+// import * as cases from './cases';
+// export { cases };
+export {
+  setMinifigForm
+} from './action';
+export { types } from './types';
+export default minifigFormReducer;
