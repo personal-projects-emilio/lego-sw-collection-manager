cba8d537f5336f248a0ac58c903daae7093d959b
Emilio Cadario <Emilio.Cadario@gmail.com>
2019-05-08 16:45:31 +0200

    Add two buttons in Miscellaneous

--- src/components/pages/minifigs/minifig-menu/miscellaneous/Miscellaneous.container.js
+++ src/components/pages/minifigs/minifig-menu/miscellaneous/Miscellaneous.container.js
@@ -1,14 +1,28 @@
+import { withStyles } from '@material-ui/core';
 import Miscellaneous from './Miscellaneous';
 import { connect } from 'react-redux';
 
 const mapStateToProps = state => ({
     totalNumber: state.minifigs.totalNumber,
     numberOwned: state.minifigs.numberOwned,
-    percentageOwned: state.minifigs.percentageOwned
+    percentageOwned: state.minifigs.percentageOwned,
+    minifigs: state.minifigs.minifigs
 });
 
 const mapDispatchToProps = dispatch => ({
 });
 
-export default connect(mapStateToProps, mapDispatchToProps)(Miscellaneous);
+const styles = theme => ({
+    button: {
+      margin: theme.spacing.unit
+    },
+    icon: {
+      marginLeft: theme.spacing.unit
+    },
+    linearProgress: {
+      margin: `${theme.spacing.unit}px 0`
+    },
+  });
+
+export default withStyles(styles)(connect(mapStateToProps, mapDispatchToProps)(Miscellaneous));
 
--- src/components/pages/minifigs/minifig-menu/miscellaneous/Miscellaneous.test.js
+++ src/components/pages/minifigs/minifig-menu/miscellaneous/Miscellaneous.test.js
@@ -1,19 +1,26 @@
 import React from 'react';
 import Miscellaneous from './Miscellaneous';
 import { shallow } from 'enzyme';
-import { Typography, LinearProgress, Grid, Tooltip } from "@material-ui/core";
+import { Typography, LinearProgress, Grid, Tooltip, Button, Icon } from "@material-ui/core";
 
 describe('<Miscellaneous />', () => {
   it('should render the Minifig Menu', () => {
     const props = {
       totalNumber: 1000,
       numberOwned: 900,
-      percentageOwned: 90
+      percentageOwned: 90,
+      classes: {
+        icon: 'icon',
+        button: 'button',
+        linearProgress: 'linearProgress'
+      }
     }
     const wrapper = shallow(<Miscellaneous {...props} />);
-    expect(wrapper.find(Grid)).toHaveLength(2);
+    expect(wrapper.find(Grid)).toHaveLength(3);
     expect(wrapper.find(Typography)).toHaveLength(1);
     expect(wrapper.find(LinearProgress)).toHaveLength(1);
     expect(wrapper.find(Tooltip)).toHaveLength(1);
+    expect(wrapper.find(Button)).toHaveLength(2);
+    expect(wrapper.find(Icon)).toHaveLength(2);
   });
 });
