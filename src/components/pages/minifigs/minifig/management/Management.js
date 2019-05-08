50d304e48f132cea4a4f48547076f0e982327b43
Emilio Cadario <Emilio.Cadario@gmail.com>
2019-05-08 19:21:18 +0200

    Fix a test bug and change the logic of filter message

--- src/components/pages/minifigs/minifig/management/Management.js
+++ src/components/pages/minifigs/minifig/management/Management.js
@@ -1,7 +1,17 @@
 import React, { useState } from "react";
-import PropTypes from 'prop-types';
-import { Grid, Checkbox, IconButton, Icon, Tooltip, Modal, Button, Paper, Typography } from "@material-ui/core";
-import styles from './Management.module.css';
+import PropTypes from "prop-types";
+import {
+  Grid,
+  Checkbox,
+  IconButton,
+  Icon,
+  Tooltip,
+  Modal,
+  Button,
+  Paper,
+  Typography
+} from "@material-ui/core";
+import styles from "./Management.module.css";
 
 export const management = props => {
   const [open, setOpen] = useState(false);
@@ -10,7 +20,7 @@ export const management = props => {
   const deleteHandler = () => {
     setOpen(false);
     deleteMinifig(reference);
-  }
+  };
   return (
     <Grid container justify="space-evenly">
       <Tooltip title="Possession" aria-label="Possession">
@@ -21,9 +31,11 @@ export const management = props => {
         />
       </Tooltip>
       <Tooltip title="Edit" aria-label="Edit">
-        <IconButton disabled>
-          <Icon>edit</Icon>
-        </IconButton>
+        <span>
+          <IconButton disabled>
+            <Icon>edit</Icon>
+          </IconButton>
+        </span>
       </Tooltip>
       <Tooltip title="Delete" aria-label="Delete">
         <IconButton onClick={() => setOpen(true)}>
@@ -33,26 +45,31 @@ export const management = props => {
       <Modal open={open} onClose={() => setOpen(false)}>
         <Paper className={styles.Paper}>
           <Typography variant="h6">
-            Are you sure you want to delete the {reference.toUpperCase()} minifig? This action is irreversible.
+            Are you sure you want to delete the {reference.toUpperCase()}{" "}
+            minifig? This action is irreversible.
           </Typography>
-          <Button variant="outlined" onClick={deleteHandler}>Delete</Button>
-          <Button variant="outlined" onClick={() => setOpen(false)}>Cancel</Button>
+          <Button variant="outlined" onClick={deleteHandler}>
+            Delete
+          </Button>
+          <Button variant="outlined" onClick={() => setOpen(false)}>
+            Cancel
+          </Button>
         </Paper>
       </Modal>
     </Grid>
-  )
-}
+  );
+};
 
 management.propTypes = {
-    reference: PropTypes.string.isRequired,
-    details: PropTypes.shape({
-        characterName: PropTypes.string.isRequired,
-        name: PropTypes.string.isRequired,
-        possessed: PropTypes.bool.isRequired,
-        tags: PropTypes.arrayOf(PropTypes.string)
-    }),
-    togglePossession: PropTypes.func.isRequired,
-    deleteMinifig: PropTypes.func.isRequired
-}
+  reference: PropTypes.string.isRequired,
+  details: PropTypes.shape({
+    characterName: PropTypes.string.isRequired,
+    name: PropTypes.string.isRequired,
+    possessed: PropTypes.bool.isRequired,
+    tags: PropTypes.arrayOf(PropTypes.string)
+  }),
+  togglePossession: PropTypes.func.isRequired,
+  deleteMinifig: PropTypes.func.isRequired
+};
 
 export default management;
