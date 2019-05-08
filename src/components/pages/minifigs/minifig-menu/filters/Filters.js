1530ef89064b672c6266f12363b936558ab1f5a3
Emilio Cadario <Emilio.Cadario@gmail.com>
2019-05-08 14:32:18 +0200

    Add tag selectfield filter

--- src/components/pages/minifigs/minifig-menu/filters/Filters.js
+++ src/components/pages/minifigs/minifig-menu/filters/Filters.js
@@ -1,48 +1,73 @@
-import React from 'react';
-import PropTypes from 'prop-types';
-import { Grid, TextField, MenuItem } from '@material-ui/core';
-import styles from './Filters.module.css';
-import SelectField from '../../../../commons/inputs/selectfield';
+import React from "react";
+import PropTypes from "prop-types";
+import { Grid, TextField, MenuItem } from "@material-ui/core";
+import styles from "./Filters.module.css";
+import SelectField from "../../../../commons/inputs/selectfield";
 
 export const filters = props => {
-  const { showOptions, show, setShow, characNames, characNameSelected, setCharacName, resetCharacName} = props;
+  const {
+    showOptions,
+    show,
+    setShow,
+    characNames,
+    characNameSelected,
+    setCharacName,
+    resetCharacName,
+    tags,
+    tagSelected,
+    setTag,
+    resetTag
+  } = props;
 
   const setShowHandler = e => {
     const value = e.target.value;
     value !== show && setShow(value);
-  }
+  };
   return (
     <Grid container>
-     <TextField
-      id="select-show-filter"
-      label="Show"
-      select
-      value={show}
-      variant="outlined"
-      fullWidth
-      className={styles.Selectfield}
-      onChange={setShowHandler}
-     >
-      {showOptions.map(option => (
-        <MenuItem key={`${option}-selectfield-choice`} value={option}>
-          {`${option[0].toUpperCase()}${option.slice(1)}`}
-        </MenuItem>
-      ))}
-     </TextField>
-     {characNames && (
-       <SelectField
-        classes={styles}
-        value={characNameSelected}
-        label="Character Name"
-        options={characNames.map(charac => ({
-          displayed: `${charac.name} (${charac.amount})`,
-          value: charac.name
-        }))}
-        setValue={setCharacName}
-        resetValue={resetCharacName}
-     />)}
+      <TextField
+        id="select-show-filter"
+        label="Show"
+        select
+        value={show}
+        variant="outlined"
+        fullWidth
+        className={styles.Selectfield}
+        onChange={setShowHandler}
+      >
+        {showOptions.map(option => (
+          <MenuItem key={`${option}-selectfield-choice`} value={option}>
+            {`${option[0].toUpperCase()}${option.slice(1)}`}
+          </MenuItem>
+        ))}
+      </TextField>
+      {characNames && (
+        <SelectField
+          value={characNameSelected}
+          label="Character Name"
+          options={characNames.map(charac => ({
+            displayed: `${charac.name} (${charac.amount})`,
+            value: charac.name
+          }))}
+          setValue={setCharacName}
+          resetValue={resetCharacName}
+        />
+      )}
+      {tags && (
+        <SelectField
+          value={tagSelected}
+          label="Tag"
+          options={tags.map(tag => ({
+            displayed: `${tag.name} (${tag.amount})`,
+            value: tag.name
+          }))}
+          setValue={setTag}
+          resetValue={resetTag}
+        />
+      )}
     </Grid>
-)}
+  );
+};
 
 filters.propTypes = {
   showOptions: PropTypes.arrayOf(PropTypes.string.isRequired).isRequired,
@@ -56,7 +81,6 @@ filters.propTypes = {
   characNames: PropTypes.array,
   setCharacName: PropTypes.func.isRequired,
   resetCharacName: PropTypes.func.isRequired
-}
-
+};
 
 export default filters;
